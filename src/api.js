88283afa36f752626f3e11db251986b6d001234e
Devon Seger <devon.seger@gmail.com>
2024-07-22 23:54:30 -0500

    change api url again

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://inventoryManagement-api.onrender.com";
+const API_URL = "https://inventorymanagement-xkjy.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
